536b7f547fe23e12951f20fcb0ec231e76bf62c7
block_zhang <zhangchuanchuan@donview.cn>
2019-12-20 15:22:09 +0800

    first commit 0.0.1

---
+++ ZCView/ZCView.h
@@ -0,0 +1,17 @@
+//
+//  ZCView.h
+//  AdsD
+//
+//  Created by Donview Dev on 2019/12/20.
+//  Copyright © 2019 Block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZCView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZCView/ZCView.m
@@ -0,0 +1,27 @@
+//
+//  ZCView.m
+//  AdsD
+//
+//  Created by Donview Dev on 2019/12/20.
+//  Copyright © 2019 Block. All rights reserved.
+//
+
+#import "ZCView.h"
+
+@implementation ZCView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self setupUI];
+    }
+    return self;
+}
+
+- (void)setupUI {
+    UILabel *lab = [[UILabel alloc] initWithFrame:self.frame];
+    lab.text = @"hello";
+    [self addSubview:lab];
+    lab.textAlignment = NSTextAlignmentCenter;
+}
+
+@end
